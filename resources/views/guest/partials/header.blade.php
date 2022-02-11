c913b2b7399e0c04ecafabd8328a8f21e0a4f3ad
Francesco-Ostellari <francescoostellari@gmail.com>
2022-02-11 16:17:58 +0100

    add-header

---
+++ resources/views/guest/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+  <div class="header-top">
+    <span class="dc-power">
+      dc power&#8480; visa&reg; 
+    </span>
+    <span class="additional-dc">
+      additional dc sites <i class="fa-solid fa-caret-down"></i>
+    </span>
+  </div>
+  <div class="header-bottom">
+      <img src="../assets/img/dc-logo.png" alt="">
+      <ul>
+        <li> <a href="#">characters</a> </li>
+        <li class="li-blue"> <a class="li-blue" href="#">comics</a> </li>
+        <li> <a href="#">movies</a> </li>
+        <li> <a href="#">tv</a> </li>
+        <li> <a href="#">games</a></li>
+        <li> <a href="#">collectibles</a> </li>
+        <li> <a href="#">videos</a> </li>
+        <li> <a href="#">fans</a> </li>
+        <li> <a href="#">news</a> </li>
+        <li> <a href="#">shop</a> </li>
+      </ul>
+    </div>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('guest.home');
 });
 
 $data = ['comics' => config('comics'), 'nomePagina' => 'DC Comis'];
